723097cdaa214a6b35e08f108ad55d1704335bad
Antony Parish <antonyparish@Antonys-MacBook-Pro.local>
2017-12-05 17:37:51 +0000

    Added route for single todo

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,41 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5a25c1989c91f35dd454a1f1';
+
+if (!ObjectID.isValid(id)) {
+  return console.log('Invalid ID');
+};
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos:', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo:', todo);
+// });
+//
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('ID not found.');
+//   }
+//   console.log('Todo by ID:', todo);
+// }).catch((e) => {
+//   consoe.log(e);
+// });
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('User not found');
+  }
+  console.log('Found user', user.email);
+}).catch((e) => {
+  console.log(e);
+});
